a5732c1a4c1c9c1a2666370ef67e02f6172c940c
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2020-07-17 22:20:11 +0530

    changing About

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,23 @@ import './Card.css'
 
 const Card = () => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow'>
-			<h1>About</h1>
-			<img alt='MyPhoto' src={Photo} />
-			<div>
-				<h2>My_Name</h2>
+		<div className='bg-light-green dib br3 pa4 ma2 grow'>
+			<h1>Profile</h1>
+			<div className='flex'>
+			<img alt='MyPhoto' src={Photo}/>
+			<div >
+				<h2 className='ma4 flex'>My_Name</h2>
+				<p>Developer || Competitive programmer</p>
 				<p>Don't know what to write about ourself. But ofcourse we are learn coding and development skills.</p>
+				<p>A Developer with strong passion for self-improvement.</p>
+				<p>I have strong Problem Solving skills & bring energy, patience, positivity and 
+				cultural values to the team.</p>
+			</div>
+			<div>
+			    <p>Name: My_Name</p>
+			    <p>Email: xyz@gmail.com</p>
+			    <p>Linkdin: xyz</p>
+			</div>
 			</div>
 		</div>
 	);
