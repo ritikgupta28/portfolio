d104926a0319126ad3d8d4b4260c7bc16613fbdc
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2020-07-18 02:21:08 +0530

    changing text alingnment in about

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,19 +4,19 @@ import './ProfileCard.css'
 
 const Card = () => {
 	return (
-		<div className='bg-light-green dib br3 pa4 ma2 grow'>
+		<div className='bg-light-green dib br3 pa5 ma5 grow'>
 			<h1>Profile</h1>
 			<div className='flex'>
 			<img alt='MyPhoto' src={Photo}/>
 			<div >
-				<h2 className='ma4 flex'>My_Name</h2>
-				<p>Developer || Competitive programmer</p>
-				<p>Don't know what to write about ourself. But ofcourse we are learn coding and development skills.</p>
-				<p>A Developer with strong passion for self-improvement.</p>
-				<p>I have strong Problem Solving skills & bring energy, patience, positivity and 
+				<h2 className='ma4 pt'>My_Name</h2>
+				<p className='tx'>Developer || Competitive programmer</p>
+				<p className='tx'>Don't know what to write about ourself. But ofcourse we are learn coding and development skills.
+				A Developer with strong passion for self-improvement.
+				I have strong Problem Solving skills & bring energy, patience, positivity and 
 				cultural values to the team.</p>
 			</div>
-			<div>
+			<div className=''>
 			    <p>Name: My_Name</p>
 			    <p>Email: xyz@gmail.com</p>
 			    <p>Linkdin: xyz</p>
