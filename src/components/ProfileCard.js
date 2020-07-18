df39c1342185fd3a290f6734a96f1b077e7aa775
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2020-07-18 17:04:20 +0530

    channging styles and photos of about

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,22 +4,23 @@ import './ProfileCard.css'
 
 const Card = () => {
 	return (
-		<div className='bg-light-green dib br3 pa5 ma5 grow'>
-			<h1>Profile</h1>
-			<div className='flex'>
+		<div className='dib br3 pa5 grow co'>
+			<h1 className='pro'>Profile</h1>
+			<div className='con'>
 			<img alt='MyPhoto' src={Photo}/>
 			<div >
-				<h2 className='ma4 pt'>My_Name</h2>
+				<h2 className='tl'>My_Name</h2>
 				<p className='tx'>Developer || Competitive programmer</p>
 				<p className='tx'>Don't know what to write about ourself. But ofcourse we are learn coding and development skills.
 				A Developer with strong passion for self-improvement.
 				I have strong Problem Solving skills & bring energy, patience, positivity and 
 				cultural values to the team.</p>
 			</div>
-			<div className=''>
+			<div className='vl'></div>
+			<div className='pt'>
 			    <p>Name: My_Name</p>
-			    <p>Email: xyz@gmail.com</p>
-			    <p>Linkdin: xyz</p>
+			    <p>Email:xyz@gmail.com</p>
+			    <p>Linkdin: @xyz</p>
 			</div>
 			</div>
 		</div>
