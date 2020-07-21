0ecd2cebeeb2435c7dcdf0da434dcf54bd967c5a
Ritik Gupta <ritikgupta2003@gmail.com>
2020-07-21 16:21:13 +0530

    update resume

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -7,21 +7,21 @@ const Card = () => {
 		<div className='dib br3 pa5 grow co'>
 			<h1 className='pro'>Profile</h1>
 			<div className='con'>
-			<img alt='MyPhoto' src={Photo}/>
-			<div >
-				<h2 className='tl'>My_Name</h2>
-				<p className='tx'>Developer || Competitive programmer</p>
-				<p className='tx'>Don't know what to write about ourself. But ofcourse we are learn coding and development skills.
-				A Developer with strong passion for self-improvement.
-				I have strong Problem Solving skills & bring energy, patience, positivity and 
-				cultural values to the team.</p>
-			</div>
-			<div className='vl'></div>
-			<div className='pt'>
-			    <p>Name: My_Name</p>
-			    <p>Email:xyz@gmail.com</p>
-			    <p>Linkdin: @xyz</p>
-			</div>
+				<img alt='MyPhoto' src={Photo}/>
+				<div >
+					<h2 className='tl'>My_Name</h2>
+					<p className='tx'>Developer || Competitive programmer</p>
+					<p className='tx'>Don't know what to write about ourself. But ofcourse we are learn coding and development skills.
+					A Developer with strong passion for self-improvement.
+					I have strong Problem Solving skills & bring energy, patience, positivity and 
+					cultural values to the team.</p>
+				</div>
+				<div className='vl'></div>
+				<div className='pt'>
+			    	<p>Name: My_Name</p>
+			    	<p>Email:xyz@gmail.com</p>
+			    	<p>Linkdin: @xyz</p>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/ResumeCard.js
+++ src/components/ResumeCard.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import './ResumeCard.css'
 
 export default class Resume extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Resume</h1> 
+			<div className='dib br3 pa5 grow co'>
+				<h1 className='pro'>Resume</h1>
+				<div className='con'>
+					<div className='lSide'>
+						<h3>PROJECTS</h3>
+						<div className='sec'>
+							<h4>Google</h4>
+							<p>This is a google assistant project</p>
+						</div>
+						<div className='sec'>
+							<h4>Portfolio</h4>
+							<p>This is a portfolio project</p>
+						</div>
+						<div className='sec'>
+							<h4>E-commerce</h4>
+							<p>This is a e-commerce website</p>
+						</div>
+						<h3>EDUCATION</h3>
+						<div className='sec'>
+							<h4>JC Bose University of Science & Technology, YMCA</h4>
+							<p>B.Tech-CE  |  CGPA: X.XX/10.00</p>
+						</div>
+						<div className='sec'>
+							<h4>School Name</h4>
+							<p>12th  |  CBSE  |  Percentage: XX.XX/100.00</p>
+						</div>
+						<div className='sec'>
+							<h4>School Name</h4>
+							<p>10th  |  CBSE  |  CGPA: X.XX/10.00</p>
+						</div>
+						<h3>ACHIEVEMENTS</h3>
+						<div className='sec'>
+							<li>3* rating on codechef.</li>
+						</div>
+					</div>
+					<div className='rSide'>
+						<h3>CODING SKILLS</h3>
+						<div className='sec'>
+							<li>C</li>
+							<li>C++</li>
+							<li>Javascript</li>
+						</div>
+						<h3>DEV SKILLS</h3>
+						<div className='sec'>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>React</li>
+							<li>Node.js</li>
+							<li>NoSql</li>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
