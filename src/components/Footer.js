31743d6598be5dff7eac502484bce0a22d482c71
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2020-07-22 13:02:56 +0530

    changing order in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
 	render() {
 		return (
 			<div className='tc'>
-				<p>Made <img alt="code" src={pic} className='im' /> by Sahil & Ritik</p>
+				<p>Made <img alt="code" src={pic} className='im' /> by Ritik & Sahil</p>
 				<p>#coding #family #friends #sleep #improve #learn</p>
 			</div>
 		)
